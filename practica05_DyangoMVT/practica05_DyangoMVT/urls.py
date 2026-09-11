8c7327f8b296452601bd3c974cfd3c9f1e7fcb0a
JorgeAdrianGutierrezLopez <23040157@itdurango.edu.mx>
2026-09-11 13:55:51 -0600

    Update urls.py

--- practica05_DyangoMVT/practica05_DyangoMVT/urls.py
+++ practica05_DyangoMVT/practica05_DyangoMVT/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(template_name='usuarios/login.html'), name='login'),  # <-- 2. Agrégalo aquí
     path('', include('usuarios.urls')),
 ]
